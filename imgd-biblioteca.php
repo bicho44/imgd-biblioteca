9c54b64d13b847b099ab1ecfc775a1147e3861da
Federico Reinoso <bicho44@gmail.com>
2021-03-04 21:25:57 -0300

    estructura del plug-in

--- imgd-biblioteca.php
+++ imgd-biblioteca.php
@@ -3,11 +3,23 @@
 Plugin Name: IMGD Biblioteca
 Plugin URI: https://github.com/bicho44/imgd-biblioteca
 Description: Pequeño plug-in para biblioteca de datos
-Version: 1.01
+Version: 1.02
 Author: Federico Reinoso
 Author URI: https://imgdigital.com.ar
 Plugin Type: Piklist
 License: GPL2
 */
+
+//Check Piklist
+add_action('init', 'my_init_function');
+function my_init_function(){
+  if(is_admin()){
+   include_once('parts/class-piklist-checker.php');
+ 
+   if (!piklist_checker::check(__FILE__)){
+     return;
+   }
+  }
+}
 
 ?>
