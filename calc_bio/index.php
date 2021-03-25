0827ab226aded727f44f0853f8dc5c01d8e3411f
Federico Reinoso <bicho44@gmail.com>
2021-03-25 17:04:48 -0300

    actualización de los menus

--- calc_bio/index.php
+++ calc_bio/index.php
@@ -4,9 +4,9 @@ include_once 'do/form-selects.php';
 function showCalculadora(){
 
     $form = "<form>";      
-    $form .="<label>Tipo de Combustible a reemplazar: ". calc_combustibles()."</label>";
-    $form .="<label>Potencia: ".calc_potencias()."</label>";
-    $form .="<label>Uso de la caldera: ".calc_calderas()."</label>";
+    $form .="<label>Tipo de Combustible a reemplazar: ".get_calc_combustibles()."</label>";
+    $form .="<label>Potencia: ".get_calc_potencias()."</label>";
+    $form .="<label>Uso de la caldera: ".get_calc_calderas()."</label>";
     $form .="<label>Precio de Combustible a reemplazar: <input type='text' id='precio' /></label>";
     $form .="<label>Horas de uso anuales: <input type='text' id='horas' /></label>";
     $form .="<input type='button' class='primary' value='Siguiente' onclick='goResultados();'/>";
