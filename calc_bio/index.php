7cdd4bd94ead0bf09045f69370ff59afc5b0f38c
Federico Reinoso <bicho44@gmail.com>
2021-03-25 17:02:29 -0300

    rediseño del formulario

--- calc_bio/index.php
+++ calc_bio/index.php
@@ -3,60 +3,17 @@ include_once 'do/form-selects.php';
 
 function showCalculadora(){
 
-?>
-<form>
-    <script type="text/javascript" src="bio.js"></script>
-    <table>
-        <tr>
-            <td>Tipo de Combustible a reemplazar</td>
-            <td>
-                <?php
-                calc_combustibles();
-                ?>
-            </td>
-        </tr>
+    $form = "<form>";      
+    $form .="<label>Tipo de Combustible a reemplazar: ". calc_combustibles()."</label>";
+    $form .="<label>Potencia: ".calc_potencias()."</label>";
+    $form .="<label>Uso de la caldera: ".calc_calderas()."</label>";
+    $form .="<label>Precio de Combustible a reemplazar: <input type='text' id='precio' /></label>";
+    $form .="<label>Horas de uso anuales: <input type='text' id='horas' /></label>";
+    $form .="<input type='button' class='primary' value='Siguiente' onclick='goResultados();'/>";
 
-        <tr>
-            <td>Potencia</td>
-            <td>
-                <?php
-                calc_potencias();
-                ?>
-            </td>
-        </tr>
-
-        <tr>
-            <td>Uso de la caldera</td>
-            <td>
-                <?php
-                calc_calderas();
-                ?>
-            </td>
-        </tr>
-        <tr>
-            <td>Precio de Combustible a reemplazar</td>
-            <td>
-                <input type="text" id="precio" />
-            </td>
-        </tr>
-        <tr>
-            <td>Horas de uso anuales</td>
-            <td>
-                <input type="text" id="horas" />
-            </td>
-        </tr>
-
-        <tr>
-            <td></td>
-            <td>
-                <input type="button" class="primary" value="Siguiente" onclick="goResultados();"/>
-            </td>
-        </tr>
-
-
-    </table>
-</form>
-<?php } 
+    $form .="</form>";
+    return $form;
+}
 
 add_shortcode( 'showCalculadora', 'showCalculadora' );
 
