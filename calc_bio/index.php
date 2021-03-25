5304c8c5344ef14d73283803baa28345c49400b3
Federico Reinoso <bicho44@gmail.com>
2021-03-25 17:44:24 -0300

    re-formateo del formulario

--- calc_bio/index.php
+++ calc_bio/index.php
@@ -3,16 +3,42 @@ include_once 'do/form-selects.php';
 
 function showCalculadora(){
 
-    $form = "<form action='#'>";
+    $form = '<form action="#" class="wpcf7-form init">';
 
-        $form .="<div><label for='combustibles'>Tipo de Combustible a reemplazar: ".get_calc_combustibles()."</label></div>";
-        $form .="<div><label for='potencias'>Potencia: ".get_calc_potencias()."</label></div>";
-        $form .="<div><label for='caldera'>Uso de la caldera: ".get_calc_calderas()."</label></div>";
-        $form .="<div><label for='precio'>Precio de Combustible a reemplazar: <input type='text' id='precio' /></label></div>";
-        $form .="<div><label for='horas'>Horas de uso anuales: <input type='text' id='horas' /></label></div>";
-        $form .="<div><input type='button' class='primary' value='"._x('Calcular','imgd')."' onclick='goResultados();'/></div>";
+        $form .='<label for="combustibles">
+        Tipo de Combustible a reemplazar:
+        <span class="wpcf7-form-control-wrap">'
+            .get_calc_combustibles().
+            '</span></label>';
 
-    $form .="</form>";
+        $form .='<label for="potencias">Potencia: 
+            <span class="wpcf7-form-control-wrap">'
+            .get_calc_potencias().
+            '</span></label>';
+
+
+        $form .='<label for="caldera">Uso de la caldera: 
+                <span class="wpcf7-form-control-wrap">'
+                .get_calc_calderas().
+                '</span>
+            </label>';
+
+
+        $form .='<label for="precio">Precio de Combustible a reemplazar:
+                    <span class="wpcf7-form-control-wrap">
+                        <input type="text" id="precio" class="wpcf7-form-control wpcf7-select" aria-required="true" aria-invalid="false"/>
+                    </span>
+                </label>';
+
+        $form .='<label for="horas">Horas de uso anuales:
+                <span class="wpcf7-form-control-wrap"> 
+                <input type="text" id="horas" />
+                </span></label>';
+
+        $form .='<input type="button" class="wpcf7-form-control wpcf7-submit" value="'._x('Calcular','imgd').'" onclick="goResultados();"/>';
+
+    $form .='</form>';
+    
     return $form;
 }
 
