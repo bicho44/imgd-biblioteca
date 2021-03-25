e8bd592c543b4de37a68bba92f37223e6a6b93da
Federico Reinoso <bicho44@gmail.com>
2021-03-25 17:22:11 -0300

    Update index.php

--- calc_bio/index.php
+++ calc_bio/index.php
@@ -10,7 +10,7 @@ function showCalculadora(){
         $form .="<div><label for='caldera'>Uso de la caldera: ".get_calc_calderas()."</label></div>";
         $form .="<div><label for='precio'>Precio de Combustible a reemplazar: <input type='text' id='precio' /></label></div>";
         $form .="<div><label for='horas'>Horas de uso anuales: <input type='text' id='horas' /></label></div>";
-        $form .="<div><input type='button' class='primary' value='"._x('Calcular','imgd')." onclick='goResultados();'/></div>";
+        $form .="<div><input type='button' class='primary' value='"._x('Calcular','imgd')."' onclick='goResultados();'/></div>";
 
     $form .="</form>";
     return $form;
