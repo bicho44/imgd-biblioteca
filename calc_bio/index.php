91880ea795441deacaa59e7c192a4be86dc50bb0
Federico Reinoso <bicho44@gmail.com>
2021-03-25 17:20:35 -0300

    actualizacion de formulario

--- calc_bio/index.php
+++ calc_bio/index.php
@@ -3,14 +3,14 @@ include_once 'do/form-selects.php';
 
 function showCalculadora(){
 
-    $form = "<form>";
+    $form = "<form action='#'>";
 
-        $form .="<div><label>Tipo de Combustible a reemplazar: ".get_calc_combustibles()."</label></div>";
-        $form .="<div><label>Potencia: ".get_calc_potencias()."</label></div>";
-        $form .="<div><label>Uso de la caldera: ".get_calc_calderas()."</label></div>";
-        $form .="<div><label>Precio de Combustible a reemplazar: <input type='text' id='precio' /></label></div>";
-        $form .="<div><label>Horas de uso anuales: <input type='text' id='horas' /></label></div>";
-        $form .="<div><input type='button' class='primary' value='Siguiente' onclick='goResultados();'/></div>";
+        $form .="<div><label for='combustibles'>Tipo de Combustible a reemplazar: ".get_calc_combustibles()."</label></div>";
+        $form .="<div><label for='potencias'>Potencia: ".get_calc_potencias()."</label></div>";
+        $form .="<div><label for='caldera'>Uso de la caldera: ".get_calc_calderas()."</label></div>";
+        $form .="<div><label for='precio'>Precio de Combustible a reemplazar: <input type='text' id='precio' /></label></div>";
+        $form .="<div><label for='horas'>Horas de uso anuales: <input type='text' id='horas' /></label></div>";
+        $form .="<div><input type='button' class='primary' value='"._x('Calcular','imgd')." onclick='goResultados();'/></div>";
 
     $form .="</form>";
     return $form;
