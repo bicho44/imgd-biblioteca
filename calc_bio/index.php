c592556b74cb7bf7271642fb4f9e050b35586c06
Federico Reinoso <bicho44@gmail.com>
2021-03-26 17:47:14 -0300

    Update index.php

--- calc_bio/index.php
+++ calc_bio/index.php
@@ -6,7 +6,7 @@ function showCalculadora(){
     // Bio JS
     wp_register_script('img_calc_bio', plugin_dir_url( __FILE__ ).'calc_bio/bio.js', false, null, false);
 
-    $form ='<script type="text/javascript" src="'.plugin_dir_url( __FILE__ ).'calc_bio/bio.js'.'"></script>';
+    $form ='<script type="text/javascript" src="'.plugin_dir_url( __FILE__ )."calc_bio/bio.js".'"></script>';
 
     $form .= '<form action="" method="get" class="wpcf7-form init">';
 
